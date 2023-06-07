c0c70087092497b43f4d4b64e6d5e4b22b7f9e03
MRawlani <rawlani@uw.edu>
2023-06-06 19:11:48 -0700

    returning a df now, instead of a tuple

--- nlpmaps/bag_of_words.py
+++ nlpmaps/bag_of_words.py
@@ -11,17 +11,20 @@ def generate_bow_embeddings(df, text_column, label_column):
         label_column (str): The name of the column in df that contains the labels.
     
     Returns:
-        tuple: A tuple containing the bag-of-words embeddings and labels.
-            - embeddings (scipy.sparse.csr_matrix): The bag-of-words embeddings of the text data.
-            - labels (numpy.ndarray): The labels corresponding to the text data.
+        pandas.DataFrame: A new DataFrame with the bag-of-words embeddings and labels.
     """
     text_data = df[text_column].values.tolist()
     labels = df[label_column].values
     
-    # Create a CountVectorizer
+    # Initialize the CountVectorizer
     vectorizer = CountVectorizer()
     
-    # Fit the vectorizer on the text data and transform the data into bag-of-words embeddings
+    # Fit the vectorizer and transform the text data into bag-of-words embeddings
     embeddings = vectorizer.fit_transform(text_data)
     
-    return embeddings, labels
+    # Create a new DataFrame with embeddings and labels
+    embeddings_df = pd.DataFrame(embeddings.toarray())
+    embeddings_df.columns = vectorizer.get_feature_names_out()
+    embeddings_df[label_column] = labels
+    
+    return embeddings_df
